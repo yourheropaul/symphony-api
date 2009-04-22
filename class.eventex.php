82feb7f990a21875d6ebdb0d5e75aa7c5474bc01
Your Hero Paul <paul.garrett@airlock.com>
2009-04-22 11:51:32 +0100

    Added two-pass value parsing to form values.

--- class.eventex.php
+++ class.eventex.php
@@ -22,6 +22,9 @@ abstract class EventEx extends Event
 	const XML_TYPE_SYMPHONY			= 0;
 	const XML_TYPE_SIMPLEXML		= 1;
 	const XML_TYPE_TEXT				= 2;
+	
+	// String matching
+	const REGEX_PLACEHOLDER			= '/([a-z-]+)\[(([^\[:]+)(?::([^\[]*))*)\]/';
 
 	/*
 	** Members 
@@ -187,8 +190,13 @@ class PostModifier
  
 class GenericSectionUpdater
 {
+	private static $_current_section_evaluations = null;
+	 
 	public static function updateNamedSections( $szRootElement, $oParent, $aSectionArray  )
 	{				
+		// Store the current sections
+		self::$_current_section_evaluations = $aSectionArray;
+		
 		$compiled_result = new XMLElement($szRootElement);			
 		$oSectionUpdater = new GenericSectionUpdate($oParent);
 		
@@ -236,9 +244,14 @@ class GenericSectionUpdater
 		$oSectionUpdater->rollbackTransaction();
 		
 		// redirect if set up
-		if ($redirect )$oSectionUpdater->actionRedirect();
+		if ($redirect) $oSectionUpdater->actionRedirect();
 		
 		return $compiled_result;
+	}
+	
+	public static function isSectionBeingEvaluated( $szSectionName )
+	{
+		return in_array( $szSectionName, self::$_current_section_evaluations);
 	}
 }
 
@@ -256,6 +269,7 @@ Class GenericSectionUpdate extends Event
 	
 	// Second pass associations
 	private $unresolved_links = array();
+	private static $temporary_unreolved_links = null;
 	
 	// store the redirect string
 	private $redirect_url = null;
@@ -321,6 +335,120 @@ Class GenericSectionUpdate extends Event
 		return $results;
 	}
 	
+	/*
+	** Handle the internal value linking
+	*/
+	
+	// try and insert a dynamic value
+	private static function __substitutePostValue( $old_key, $new_key, $value )
+	{
+		unset($_POST['fields'][$old_key]);
+		$_POST['fields'][$new_key] = $value;		
+	}
+	
+	// For backwards-compatability; maintain double arrow syntax
+	private function __handleLinks( $key, $iSectionID, $szPostKey, &$local_unresolved_links)
+	{		
+		$parts = explode("=>", $key);
+						
+		// FIXME: separate this, use for system:id below
+		if (count($parts) > 1)
+		{
+			$new_key = trim($parts[0]);
+			
+			$target_handle = trim($parts[1]);
+				
+			// More than one target entry means many-to-many; only one is one-to-many
+			$target_index = (count($this->updated_entries[$target_handle]) > 1 ? $this->updated_counts[$szPostKey] : 0);
+											
+			$value = $this->updated_entries[$target_handle][$target_index];						
+				
+			if ($value)
+			{
+				self::__substitutePostValue($key, $new_key, $value);
+			}
+			else
+			{
+				// add this to a list for the second pass
+				$local_unresolved_links[] = array( 'section-id' => $iSectionID, 
+                                                    'target-handle' => $target_handle, 
+                                                    'target-index' => $target_index,
+                                                    'field-name' => $new_key 
+                                                 );
+			}	
+			
+			// Make sure nothing else happens
+			return;
+		}					
+	}
+	
+	// Replace section[field] placeholders with their proper values 
+	private function __processPlaceholders( $key, $iSectionID, $szPostKey, &$local_unresolved_links)
+	{
+		// reset the temporary container
+		self::$temporary_unreolved_links = array();
+		
+		// Disgiuse escaped hyphens
+		$_POST['fields'][$key] = preg_replace('/\\\\-/', '{@hyphen@}', $_POST['fields'][$key]);
+		
+		// Run all the callbacks
+		$_POST['fields'][$key] = preg_replace_callback(EventEx::REGEX_PLACEHOLDER, array($this, 'placeholderCallback'), $_POST['fields'][$key]);
+		
+		// Un-disguise escaped hyphens
+		$_POST['fields'][$key] = preg_replace('/{@hyphen@}/', '-', $_POST['fields'][$key]);				
+		
+		// Set up any unresolved links
+		foreach (self::$temporary_unreolved_links as $link)
+		{			
+			// Update the permanent placeholder
+			$_POST[$szPostKey][$key] = $_POST['fields'][$key];		
+			
+			// Determine the target index
+			$target_index = (count($this->updated_entries[$link['target-handle']]) > 1 ? $this->updated_counts[$szPostKey] : 0);
+			
+			// Add to the section-level list of missed links
+			$local_unresolved_links[] = array( 'section-id' => $iSectionID, 
+                                               'target-handle' => $link['target-handle'], 
+                                               'target-index' => $target_index,
+                                               'field-name' => $link['field-name'], 
+                                               'replacement-key' => $link['replacement-key'],
+                                               'this-postkey' => $szPostKey,
+                                               'this-key' => $key
+                                             );
+		}
+	}
+	
+	// Callback for the replacement above
+	public function placeholderCallback( $matches )
+	{		
+		// Only sections defined in the event can be referenced
+		if (!GenericSectionUpdater::isSectionBeingEvaluated($matches[1]))
+			return $matches[0];
+			
+		// System:id links
+		if ($matches[2] == 'system:id')
+		{
+			if (isset( $_POST[$matches[1]]['system:id']))
+				return $_POST[$matches[1]]['system:id'];
+			
+			$replacement_key = '{@system-id:'.$matches[1].'@}';
+			
+			self::$temporary_unreolved_links[] =  array( 
+                                                    'target-handle' => $matches[1],
+                                                    'field-name' => $matches[2],
+                                                    'replacement-key' => $replacement_key                                                                                                       
+                                                 );
+			
+			return $replacement_key;
+		}
+	
+		return $_POST[$matches[1]][$matches[2]];	
+	}
+	
+	/*
+	** Master update method 
+	*/
+	
 	public function updateSection( $iSectionID, $szPostKey, $aEntry, $aFiles, $field_array, $index_key = null )
 	{
 		// Fake the getSource() method
@@ -383,39 +511,12 @@ Class GenericSectionUpdate extends Event
 				// Remove any traing seperators
 				$_POST['fields'][$key] = preg_replace('/'.$separationChar.'$/','',$_POST['fields'][$key]);
 			}
+					
+			// Deal with linked fields (now deprecated)	
+			$this->__handleLinks( $key, $iSectionID, $szPostKey, $local_unresolved_links );		
 			
-			/*
-			** Deal with linked fields
-			*/
-			
-			$parts = explode("=>", $key);
-			
-			if (count($parts) > 1)
-			{
-				$new_key = trim($parts[0]);
-				
-				$target_handle = trim($parts[1]);
-				
-				// More than one target entry means many-to-many; only one is one-to-many
-				$target_index = (count($this->updated_entries[$target_handle]) > 1 ? $this->updated_counts[$szPostKey] : 0);
-												
-				$value = $this->updated_entries[$target_handle][$target_index];
-				
-				if ($value)
-				{
-					unset($_POST['fields'][$key]);
-					$_POST['fields'][$new_key] = $value;
-				}
-				else
-				{
-					// add this to a list for the second pass
-					$local_unresolved_links[] = array( 'section-id' => $iSectionID, 
-                                                       'target-handle' => $target_handle, 
-                                                       'target-index' => $target_index,
-                                                       'field-name' => $new_key 
-                                                     );
-				}
-			}					
+			// Parse the form values
+			$this->__processPlaceholders( $key, $iSectionID, $szPostKey, $local_unresolved_links); 	
 		}
 		
 		include(TOOLKIT . '/events/event.section.php');	 
@@ -505,10 +606,18 @@ Class GenericSectionUpdate extends Event
 			// Set up the ID
 			$_POST['id'] = $link['entry-id'];
 			
-			// And pop in the field data
+			// And pop in the field data	
 			$value = $this->updated_entries[$link['target-handle']][$link['target-index']];
 			
-			$_POST['fields'][$link['field-name']] = $value; 						
+			if ($link['replacement-key'])
+			{
+				// replace the provided key with the system id
+				$_POST['fields'][$link['this-key']] = preg_replace('/' . $link['replacement-key'] . '/', $value, $_POST[$link['this-postkey']][$link['this-key']]);
+			}
+			else
+			{
+				$_POST['fields'][$link['field-name']] = $value;	
+			}	
 			
 			if ($value)
 				include(TOOLKIT . '/events/event.section.php');	 
@@ -542,7 +651,7 @@ Class GenericSectionUpdate extends Event
 	{
 		if ($this->redirect_url)
 		{
-			$this->redirect_url = preg_replace_callback('/([a-z-]+)\[(([^\[:]+)(?::([^\[]*))*)\]/', array($this, 'redirectCallback'), $this->redirect_url);
+			$this->redirect_url = preg_replace_callback(EventEx::REGEX_PLACEHOLDER, array($this, 'redirectCallback'), $this->redirect_url);
 			header("location: " . $this->redirect_url);
 			die;
 		}
